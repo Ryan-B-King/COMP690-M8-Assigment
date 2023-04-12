362e67ba38a46dbc9dbbf78707daa9fc87f8e594
Ryan King <33941531+Ryan-B-King@users.noreply.github.com>
2023-04-11 21:26:44 -0700

    completed

--- js/script-array-practice.js
+++ js/script-array-practice.js
@@ -1,38 +1,217 @@
-//STEP 1
+// //STEP 1
+const favoriteMovies = [
+  'Jurassic Park',
+  'Forrest Gump',
+  'Avengers Endgame',
+  'Dungeons & Dragons',
+  'Star Trek',
+];
 
-//STEP 2
+// console.log(favoriteMovies[1]);
 
-//STEP 3
+// //STEP 2
 
-//STEP 4
+// const movies = new Array(5);
+// favoriteMovies.forEach((movie, index) => (movies[index] = movie));
+// console.log(movies);
 
-//STEP 5
+// //STEP 3
 
-//STEP 6
+// const movies = new Array(5);
+// favoriteMovies.forEach((movie, index) => (movies[index] = movie));
+// movies.splice(2, 0, 'The Mask');
+// console.log(movies.length);
 
-//STEP 7
+// //STEP 4
 
-//STEP 8
+// const movies = [];
+// favoriteMovies.forEach((movie, index) => (movies[index] = movie));
+// delete movies[0];
 
-//STEP 9
+// console.log(movies);
 
-//STEP 10
+// //STEP 5
+// const movies = [];
+// favoriteMovies.forEach((movie, index) => (movies[index] = movie));
+// movies.push('The Mask', 'Ace Ventura: Pet Detective');
+// for (let movie in movies) {
+//   console.log(movies[movie]);
+// }
 
-//STEP 11
+// //STEP 6
 
-//STEP 12
+// const movies = [];
+// favoriteMovies.forEach((movie, index) => (movies[index] = movie));
+// movies.push('The Mask', 'Ace Ventura: Pet Detective');
+// for (let movie of movies) {
+//   console.log(movie);
+// }
 
-//STEP 13
+// //STEP 7
+// const movies = [];
+// favoriteMovies.forEach((movie, index) => (movies[index] = movie));
+// movies.push('The Mask', 'Ace Ventura: Pet Detective');
+// movies.sort();
+// for (let movie of movies) {
+//   console.log(movie);
+// }
 
-//STEP 14
+// //STEP 8
 
-//STEP 15
+// const movies = [];
+// favoriteMovies.push('The Mask', 'Ace Ventura: Pet Detective');
+// movies.push('The Mask', 'Ace Ventura: Pet Detective');
+// const leastFavMovies = ['Beaches', 'Twilight', 'Jack Frost'];
 
-//STEP 16
+// console.log('Movies I like:', '\n \n');
+// for (let movie of favoriteMovies) {
+//   console.log(movie);
+// }
 
-//STEP 17
+// console.log('\n');
 
+// console.log('Movies I regret watching:', '\n \n');
+// for (let movie of leastFavMovies) {
+//   console.log(movie);
+// }
 
-let names = ["Zak", "Fred", "Sally"];
-names = names.push("Jessica", "John");
-console.log(names.toString());
+// //STEP 9
+
+// favoriteMovies.push('The Mask', 'Ace Ventura: Pet Detective');
+// const leastFavMovies = ['Beaches', 'Twilight', 'Jack Frost'];
+
+// console.log('Movies I like:', '\n \n');
+// for (let movie in favoriteMovies) {
+//   console.log(favoriteMovies[movie]);
+// }
+
+// console.log('\n');
+
+// console.log('Movies I regret watching:', '\n \n');
+// for (let movie in leastFavMovies) {
+//   console.log(leastFavMovies[movie]);
+// }
+
+// const movies = favoriteMovies.concat(leastFavMovies);
+// movies.sort().reverse();
+// console.log(movies);
+
+// //STEP 10
+
+// favoriteMovies.push('The Mask', 'Ace Ventura: Pet Detective');
+// const leastFavMovies = ['Beaches', 'Twilight', 'Jack Frost'];
+
+// console.log('Movies I like:', '\n \n');
+// for (let movie in favoriteMovies) {
+//   console.log(favoriteMovies[movie]);
+// }
+
+// console.log('\n');
+
+// console.log('Movies I regret watching:', '\n \n');
+// for (let movie in leastFavMovies) {
+//   console.log(leastFavMovies[movie]);
+// }
+
+// const movies = favoriteMovies.concat(leastFavMovies);
+// movies.sort().reverse();
+// console.log(movies);
+// const lastItem = movies.slice(movies.length - 1, movies.length)[0];
+// console.log(lastItem);
+
+// //STEP 11
+
+// // favoriteMovies.push('The Mask', 'Ace Ventura: Pet Detective');
+// // const leastFavMovies = ['Beaches', 'Twilight', 'Jack Frost'];
+
+// // console.log('Movies I like:', '\n \n');
+// // for (let movie in favoriteMovies) {
+// //   console.log(favoriteMovies[movie]);
+// // }
+
+// // console.log('\n');
+
+// // console.log('Movies I regret watching:', '\n \n');
+// // for (let movie in leastFavMovies) {
+// //   console.log(leastFavMovies[movie]);
+// // }
+
+// // const movies = favoriteMovies.concat(leastFavMovies);
+// // movies.sort().reverse();
+// // console.log(movies);
+// // const firstItem = movies.slice(0, 1)[0];
+// // console.log(firstItem);
+
+// // //STEP 12
+
+// // const movieIndices = leastFavMovies.map((movie) => movies.indexOf(movie));
+// // const moviesILike = ['Jumanji', 'Honey, I Shrunk the Kids', 'Nutty Professor'];
+// // movieIndices.forEach((movieIndex, index) => {
+// //   movies[movieIndex] = moviesILike[index];
+// // });
+// // movies.sort().reverse();
+// // console.log(movies);
+
+// //STEP 13
+
+// // const movies = [
+// //   ['Forrest Gump', 1],
+// //   ['Jurassic Park', 2],
+// //   ['Avengers Endgame', 3],
+// //   ['Dungeons & Dragons', 4],
+// //   ['Star Trek', 5],
+// // ];
+
+// // for (let movieArray of movies) {
+// //   console.log(movieArray.filter((item) => typeof item === 'string')[0]);
+// // }
+
+// //STEP 14
+
+// // const employees = ['ZAK', 'JESSICA', 'MARK', 'FRED', 'SALLY'];
+
+// // const showEmployee = (employees) => {
+// //   employees.forEach((employee) => console.log(employee));
+// // };
+
+// // showEmployee(employees);
+
+// //STEP 15
+
+// let testData = [58, '', 'abcd', true, null, false, 0];
+
+// const filterValues = (values) => {
+//   return values.filter((value) => value);
+// };
+
+// console.log(filterValues(testData));
+
+// //STEP 16
+
+// testData = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const getRandomItem = (values) => {
+//   const index = Math.floor(Math.random() * values.length);
+//   return values[index];
+// };
+
+// console.log(getRandomItem(testData));
+
+// //STEP 17
+
+// const getLargestNumber = (nums) => {
+//   let largestNumber;
+//   nums.forEach((num) => {
+//     if (!largestNumber) {
+//       largestNumber = num;
+//     }
+
+//     if (largestNumber < num) {
+//       largestNumber = num;
+//     }
+//   });
+
+//   return largestNumber;
+// };
+
+// console.log(getLargestNumber(testData));
--- js/script-ems.js
+++ js/script-ems.js
@@ -1,59 +1,107 @@
 // CREATE AN ARRAY OF EMPLOYEES
+let employees = [];
 
+const showEmployeeCount = () => {
+  let empCount = employees.length;
+  const output = document.querySelector('output');
+  output.innerText = `(${empCount})`;
+};
 
 // CHECK TO SEE IF STORAGE OBJECT EXISTS WHEN THE PAGE LOADS
 // IF DOES, RETURN STORAGE OBJECT INTO ARRAY INSTEAD OF POPULATED ARRAY
+window.addEventListener('load', () => {
+  if (JSON.parse(localStorage.getItem('employees'))) {
+    employees = JSON.parse(localStorage.getItem('employees'));
+  } else {
+    employees = [
+      [12345678, 'ZAK', 1234, 'zak@e.com', 'Marketing'],
+      [74732652, 'JESSICA', 5678, 'jessica@e.com', 'Engineering'],
+      [53462543, 'MARK', 9012, 'mark@e.com', 'Administrative'],
+      [67546135, 'FRED', 3456, 'fred@e.com', 'Quality Assurance'],
+      [97453826, 'SALLY', 7890, 'sally@e.com', 'Executive'],
+    ];
+  }
 
+  buildGrid();
+});
 
 // GET DOM ELEMENTS
-
+const form = document.querySelector('form');
+const table = document.getElementById('empTable');
+let tbody = document.querySelector('tbody');
 
 // BUILD THE EMPLOYEES TABLE WHEN THE PAGE LOADS
 
-
 // ADD EMPLOYEE
 form.addEventListener('submit', (e) => {
-    // PREVENT FORM SUBMISSION
+  // PREVENT FORM SUBMISSION
+  e.preventDefault();
+  // GET THE VALUES FROM THE TEXT BOXES
+  const $ = (id) => document.getElementById(id);
+  const id = $('id').value;
+  const name = $('name').value;
+  const extension = $('extension').value;
+  const email = $('email').value;
+  const department = $('department').value;
 
-    // GET THE VALUES FROM THE TEXT BOXES
+  // ADD THE NEW EMPLOYEE TO A NEW ARRAY OBJECT
+  const newEmployee = [id, name, extension, email, department];
 
-    // ADD THE NEW EMPLOYEE TO A NEW ARRAY OBJECT
+  // PUSH THE NEW ARRAY TO THE *EXISTING* EMPLOYEES ARRAY
+  employees.push(newEmployee);
 
-    // PUSH THE NEW ARRAY TO THE *EXISTING* EMPLOYEES ARRAY
+  // BUILD THE GRID
+  buildGrid();
 
-    // BUILD THE GRID
+  // RESET THE FORM
+  form.reset();
 
-    // RESET THE FORM
-
-    // SET FOCUS BACK TO THE ID TEXT BOX
-
+  // SET FOCUS BACK TO THE ID TEXT BOX
+  document.getElementById('id').focus();
 });
 
 // DELETE EMPLOYEE
 empTable.addEventListener('click', (e) => {
-    // CONFIRM THE DELETE
+  // CONFIRM THE DELETE
+  if (confirm(`Are you sure you want to delete this row?`)) {
+    // GET THE SELECTED ROWINDEX FOR THE TR (PARENTNODE.PARENTNODE)
+    const rowIndex = e.target.parentNode.rowIndex;
 
-        // GET THE SELECTED ROWINDEX FOR THE TR (PARENTNODE.PARENTNODE)
+    // REMOVE EMPLOYEE FROM ARRAY
+    employees.splice(rowIndex - 1, 1);
 
-        // REMOVE EMPLOYEE FROM ARRAY
-
-        // BUILD THE GRID
-
+    // BUILD THE GRID
+    buildGrid();
+  }
 });
 
 // BUILD THE EMPLOYEES GRID
 function buildGrid() {
-    // REMOVE THE EXISTING SET OF ROWS BY REMOVING THE ENTIRE TBODY SECTION
+  // REMOVE THE EXISTING SET OF ROWS BY REMOVING THE ENTIRE TBODY SECTION
+  tbody.remove();
 
-    // REBUILD THE TBODY FROM SCRATCH
+  // REBUILD THE TBODY FROM SCRATCH
+  // LOOP THROUGH THE ARRAY OF EMPLOYEES
+  // REBUILDING THE ROW STRUCTURE
+  // BIND THE TBODY TO THE EMPLOYEE TABLE
+  table.appendChild(document.createElement('tbody'));
+  tbody = document.querySelector('tbody');
 
-    // LOOP THROUGH THE ARRAY OF EMPLOYEES
-    // REBUILDING THE ROW STRUCTURE
+  for (let employee of employees) {
+    const row = document.createElement('tr');
+    row.innerHTML = `<td>${employee[0]}</td>
+    <td>${employee[1]}</td>
+    <td>${employee[2]}</td>
+    <td>${employee[3]}</td>
+    <td>${employee[4]}</td>
+    `;
 
-    // BIND THE TBODY TO THE EMPLOYEE TABLE
+    tbody.appendChild(row);
+  }
 
-    // UPDATE EMPLOYEE COUNT
+  // UPDATE EMPLOYEE COUNT
+  showEmployeeCount();
 
-    // STORE THE ARRAY IN STORAGE
-
-};
+  // STORE THE ARRAY IN STORAGE
+  localStorage.setItem('employees', JSON.stringify(employees));
+}
